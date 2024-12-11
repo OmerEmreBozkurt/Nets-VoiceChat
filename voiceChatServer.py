5b69e927d8c1f06132c68dc6167528bf66250ded
Ömer Emre Bozkurt <omeremre34@hotmail.com>
2024-12-12 02:32:40 +0300

    init comm

---
+++ voiceChatServer.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+port = 5000
+host = "0.0.0.0"
+
+server = socket.socket()
+
+server.bind((host, port))
+
+server.listen(5)
+
+client = []
+
+def start():
+    while True:
+        conn, addr = server.accept()
+        print("Client connected from:", addr)  # Add this line
+        client.append(conn)
+        t = threading.Thread(target=send, args=(conn,))
+        t.start()
+
+def send(fromConnection):
+    try:
+        while True:
+            data = fromConnection.recv(4096)
+            if data:
+                print("Received data from a client.")  # Add this line
+                for cl in client:
+                    if cl != fromConnection:
+                        cl.send(data)
+            else:
+                break
+    except:
+        print("Client Disconnected")
+
+start()
